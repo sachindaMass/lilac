d31e292eabbf3df9dad4082913f5b17254433455
Sachinda <sachinda46@gmail.com>
2023-07-20 04:10:08 +0530

    set up & build to navbar

---
+++ src/customer/pages/homePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeCarousel from '../../components/navigation/homeCarousel/HomeCarousel';
+
+function HomePage() {
+  return (
+    <div>
+      <HomeCarousel />
+      <div>other section</div>
+    </div>
+  );
+}
+
+export default HomePage;
